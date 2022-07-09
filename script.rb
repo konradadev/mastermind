6d87f7103e65889661e31471c9c740df007e2696
konradadev <konradadev@gmail.com>
2022-07-09 15:54:31 +0200

    Add initial gameplay loop

--- script.rb
+++ script.rb
@@ -1,5 +1,25 @@
 class Game
+  attr_writer :guess
+
   def initialize
-    @code = 0000
+    @code = '1234'
+    @guess = ''
   end
+
+  def codes_match?
+    @code == @guess
+  end
+
 end
+
+def gameplay_code_breaker
+  game = Game.new
+  while game.codes_match? == false
+    puts 'Try to guess the code:'
+    guess = gets.chomp
+    game.guess = guess
+  end
+  puts 'You won'
+end
+
+gameplay_code_breaker
